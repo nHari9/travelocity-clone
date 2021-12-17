62cb61e40ec46c9cc74c3fd3860ce1f6c9840f5f
nHari9 <nharikishorereddy9@gmail.com>
2021-12-17 14:25:40 +0530

    signup cred js

--- login.js
+++ login.js
@@ -2,8 +2,15 @@
 
 document.querySelector("form").addEventListener("submit",goto)
 
+var cred=JSON.parse(localStorage.getItem(signupdetails));
+
 function goto(event){
     event.preventDefault();
+
+    // var email = document.
+
+
+    // if(user)
     window.location.href="index.html";
-    // console.log("here");
 }
+
