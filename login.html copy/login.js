748513c67bfa141f6357ff672ac2c6d6203b0476
vivekkumar1994 <96063498+vivekkumar1994@users.noreply.github.com>
2021-12-18 11:31:23 +0530

    change something

---
+++ login.html copy/login.js
@@ -0,0 +1,23 @@
+document.querySelector("form").addEventListener("submit",goto)
+
+var cred=JSON.parse(localStorage.getItem("usercred"));
+
+function goto(event){
+    event.preventDefault();
+
+    var email = document.querySelector("#email").value;
+    var password=document.querySelector("#password").value;
+
+    for(i=0;i<cred.length;i++){
+        if(cred[i].mail==email && cred[i].pass==password){
+            window.location.href="index.html";
+            
+        }
+        else{
+            alert("please enter the correct details!!")
+        }
+    }
+    
+    
+    
+}
