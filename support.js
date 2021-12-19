049d1870b71806f24dedc1f4798487837bd4e6b8
ashwanisahu97 <ashsahu627@gmail.com>
2021-12-19 18:18:51 +0530

    added header and footer to support page

--- support.js
+++ support.js
@@ -1,6 +1,16 @@
+var count = 0;
+  function dropMenu() {
+    if (count == 0) {
+      document.getElementById("dropMenu").style.visibility = "visible";
+      count = 1;
+    } else if (count == 1) {
+      document.getElementById("dropMenu").style.visibility = "hidden";
+      count = 0;
+    }
+  }
 
 document.querySelector("#supportForm").addEventListener("submit",addInformation);
-var supportArr=JSON.parse(localStorage.getItem(""))||[];
+var supportArr=JSON.parse(localStorage.getItem("queries"))||[];
 function addInformation(event){
 event.preventDefault();
 var name=document.querySelector("#name").value;
@@ -17,4 +27,5 @@ query:query
 console.log(supportObj);
 supportArr.push(supportObj);
 localStorage.setItem("queries",JSON.stringify(supportArr));
+alert("Your query ticket has been generated, you will be contacted by our customer service provider soon.");
 }
