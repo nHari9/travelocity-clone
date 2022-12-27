c57e4b5e46c736287029617244c97ce8bba37759
ashwanisahu97 <ashsahu627@gmail.com>
2022-12-27 17:02:33 +0530

    signup page corner cases updated

--- signin.js
+++ signin.js
@@ -1,20 +1,24 @@
 document.querySelector("form").addEventListener("submit",goto)
 
-var cred=JSON.parse(localStorage.getItem("usercred"));
+var cred=JSON.parse(localStorage.getItem("usercred"))||[];
 
-function goto(event){
+function goto(event) {
     event.preventDefault();
-
+    console.log(cred)
     var email = document.querySelector("#email").value;
-    var password=document.querySelector("#password").value;
-
-    for(i=0;i<cred.length;i++){
-        if(cred[i].mail==email && cred[i].pass==password){
-            window.location.href="index.html";
+    var password = document.querySelector("#password").value;
+    console.log(email, password)
+    if (cred.length==0) {
+        alert("please enter the correct details!!")
+    } else{
+        for(i = 0; i<cred.length; i++) {
+        if (cred[i].mail == email && cred[i].pass == password) {
+            window.location.href = "index.html";
             
         }
-        else{
+        else {
             alert("please enter the correct details!!")
         }
     }
+}
 }
